57a2de989d40d8f99e693a624da5336d18fafab3
shreesha322 <shreesha@netbramha.com>
2024-04-19 11:36:20 +0530

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      blue: '#006DF5',
+      pink: '#D92B7B',
+      'dark-yellow': '#B36200',
+      green: '#098543',
+      purple: '#5A5AC7'
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  plugins: [],
+}
