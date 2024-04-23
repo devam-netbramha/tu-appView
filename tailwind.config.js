496dee883fce00ba8f99a2e7345e1b87299c4928
shreesha322 <shreesha@netbramha.com>
2024-04-23 14:17:17 +0530

    credit score page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,45 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
+    fontSize: {
+      lg: '24px',
+      md: '18px',
+      sm: '16px',
+      '4xl': '2.25rem',
+    },
     colors: {
+      background: '#FAFAFA',
       blue: '#006DF5',
+      cibil: '#06addc',
       pink: '#D92B7B',
-      'dark-yellow': '#B36200',
       green: '#098543',
-      purple: '#5A5AC7'
+      'office_green':'#008A00',
+      purple: '#5A5AC7',
+      grey: '#737577',
+      orange:'#F26842',
+      'dark-yellow': '#B36200',
+      'dark_grey': '#595959',
+      'dim_grey':'#525252',
+      'dark_charcoal': '#262626',
+      'light_green': '#E0F6E9',
+      'orange_peel': '#FF871F',
+      'light_sky_blue': '#E6F6FA',
+      'greyish_blue': '#F2F2FC ',
+      'light_grey': '#F2F2F2',
+      'pale_rose':'#FBE8EC'
+    },
+    fontFamily: {
+      intro: ['Intro', 'sans-serif'],
     },
     extend: {
       spacing: {
         128: '32rem',
         144: '36rem',
+        16: '16px',
       },
-      borderRadius: {
-        '4xl': '2rem',
-      },
+      // borderRadius: {
+      //   '4xl': '2rem',
+      // },
     },
   },
   plugins: [],
